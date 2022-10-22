545e2a0d76ebb979689d4d6de0e6c321ae22fa85
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-22 20:01:42 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int t,a,b,s;     cin>>t;     while(t>0)     {         s=0;         cin>>a>>b;         s=a+b;         cout<<s<<endl;         t--;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,b,s;
+    cin>>t;
+    while(t>0)
+    {
+        s=0;
+        cin>>a>>b;
+        s=a+b;
+        cout<<s<<endl;
+        t--;
+    }
+}
