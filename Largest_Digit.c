4a2729a2e7fcfec36994c11aa3dcdfcb98efabec
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-24 19:15:42 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int n,rem,max=0;     cin>>n;     while(n>0)     {         rem=n%10;         if(max<rem)         max=rem;         n=n/10;     }     cout<<max; }

---
+++ Largest_Digit.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,rem,max=0;
+    cin>>n;
+    while(n>0)
+    {
+        rem=n%10;
+        if(max<rem)
+        max=rem;
+        n=n/10;
+    }
+    cout<<max;
+}
