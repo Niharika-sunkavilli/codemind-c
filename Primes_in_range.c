6710a8f1028b2a857fc384d2513150bbd3307452
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-25 15:46:27 +0530

    #include <bits/stdc++.h> using namespace std; int prime(int n) {     int c=0;     if(n==1)     return false;     for(int i=2;i<=sqrt(n);i++)     {         if(n%i==0)         c++;     }     if(c==0)     return true;     else     return false; } int main() {     int m,n,c=0;     cin>>m;     cin>>n;     for(int i=m;i<=n;i++)     {         if(prime(i))         c++;     }     cout<<c; }

---
+++ Primes_in_range.c
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int prime(int n)
+{
+    int c=0;
+    if(n==1)
+    return false;
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+        c++;
+    }
+    if(c==0)
+    return true;
+    else
+    return false;
+}
+int main()
+{
+    int m,n,c=0;
+    cin>>m;
+    cin>>n;
+    for(int i=m;i<=n;i++)
+    {
+        if(prime(i))
+        c++;
+    }
+    cout<<c;
+}
