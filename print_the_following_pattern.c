fabb7ace2b770a30f2d84b996f4914cc152b1403
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-25 13:58:02 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     for(int i=1;i<=n;i++)     {         for(int j=n;j>=1;j--)         {             cout<<j<<" ";         }         cout<<' ';     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -6,10 +6,9 @@ int main()
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n+1-i;j++)
+        for(int j=n;j>=1;j--)
         {
-            cout<<j;
-            
+            cout<<j<<" ";
         }
         cout<<'
 ';
