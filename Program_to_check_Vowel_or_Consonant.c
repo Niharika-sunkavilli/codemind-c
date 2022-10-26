2159e91895bd71c3681dbeb0ff030ef09fd31dc2
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-26 21:23:18 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     char x;     cin>>x;     if(x=='A' || x=='E' || x=='I' || x=='O' || x=='U' || x=='a' || x=='e' || x=='i' || x=='o' || x=='u')     cout<<"Vowel";     else     cout<<"Consonant"; }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x;
+    cin>>x;
+    if(x=='A' || x=='E' || x=='I' || x=='O' || x=='U' || x=='a' || x=='e' || x=='i' || x=='o' || x=='u')
+    cout<<"Vowel";
+    else
+    cout<<"Consonant";
+}
