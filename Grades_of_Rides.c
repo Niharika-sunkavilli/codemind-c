21d0638010b047d5e260abb9a99e99f3ead8e243
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-29 23:01:38 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int hurl,spin,speed;     cin>>hurl>>spin>>speed;     if(hurl>50 && spin>60 && speed>100)     cout<<10;     else if(hurl>50 && spin>60)     cout<<9;     else if(spin>60 && speed>100)     cout<<8;     else if(hurl>50 && speed>100)     cout<<7;     else if(hurl>50 || spin>60 && speed>100)     cout<<6;     else     cout<<5;
    }

---
+++ Grades_of_Rides.c
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int hurl,spin,speed;
+    cin>>hurl>>spin>>speed;
+    if(hurl>50 && spin>60 && speed>100)
+    cout<<10;
+    else if(hurl>50 && spin>60)
+    cout<<9;
+    else if(spin>60 && speed>100)
+    cout<<8;
+    else if(hurl>50 && speed>100)
+    cout<<7;
+    else if(hurl>50 || spin>60 && speed>100)
+    cout<<6;
+    else
+    cout<<5;
+    
+}
