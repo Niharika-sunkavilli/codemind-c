c5413c8ced1ddb77f9bd56cc75d917f543064e95
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-30 20:44:38 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int n,pro=1,sum=0,rem;     cin>>n;     while(n>0)     {         rem=n%10;         sum=sum+rem;         pro=pro*rem;         n=n/10;     }     if(sum==pro)     cout<<"Spy Number";     else     cout<<"Not Spy Number"; }

---
+++ Spy_Number.c
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,pro=1,sum=0,rem;
+    cin>>n;
+    while(n>0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        pro=pro*rem;
+        n=n/10;
+    }
+    if(sum==pro)
+    cout<<"Spy Number";
+    else
+    cout<<"Not Spy Number";
+}
